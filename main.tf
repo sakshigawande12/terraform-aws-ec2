e12735dd9a5c78ade654f66ef0221d3709b33ba2
sakshigawande12 <sakshi.gawande@knoldus.com>
2021-12-07 16:16:57 +0530

    ec2 module added

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "app_server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
